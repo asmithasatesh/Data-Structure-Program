b7c87428a8702889d1bda008f0dc8559eaf27c5a
asmithasatesh <asmithasatesh@gmail.com>
2021-07-17 13:40:54 +0530

    [Asmitha]Find an element in Hash Table and store in file

--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.IO;
+using System.Numerics;
 namespace DataStructurePrograms
 {
     class Program
@@ -14,6 +15,7 @@ namespace DataStructurePrograms
             Console.WriteLine("Enter 5-Palindrome Checker");
             Console.WriteLine("Enter 6-Prime Numbers in 2D array");
             Console.WriteLine("Enter 7-Number of Binary Search Tree");
+            Console.WriteLine("Enter 8-To Search an element in Hash Table");
 
             int ch = Convert.ToInt32(Console.ReadLine());
             switch (ch)
@@ -44,10 +46,41 @@ namespace DataStructurePrograms
                 case 7:
                     NumbertofBST numbertofBST = new NumbertofBST();
                     Console.WriteLine("Enter the size of tree");
-                    int number = Convert.ToInt32(Console.ReadLine());
-                    int count=numbertofBST.NumberOfTree(number);
+                    BigInteger number = Convert.ToInt32(Console.ReadLine());
+                    BigInteger count =numbertofBST.NumberOfTree(number);
                     Console.WriteLine("Total count of BST with size {0} is: {1}", number, count);
                     break;
+                case 8:
+                    HashedSlots<int> hashedSlots = new HashedSlots<int>(10);
+                    GetInput(hashedSlots);
+                    break;
+            }
+
+        }
+        public static void GetInput(HashedSlots<int> hashedSlots)
+        {
+            string filePath = @"D:\Assignments\DataStructurePrograms\DataStructurePrograms\NumberSlots.txt";
+            string text = File.ReadAllText(filePath);
+            string[] numbers = text.Split(" ");
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                hashedSlots.AddNode(Convert.ToInt32(numbers[i]));
+            }
+            hashedSlots.Display();
+            Console.WriteLine("Enter the number to search:");
+            int searchValue = Convert.ToInt32(Console.ReadLine());
+
+            //Search element in Hash Table
+            if (hashedSlots.Search(searchValue) == 1)
+            {
+                Console.WriteLine("Value {0} is Present in Hash Table", searchValue);
+                File.WriteAllText(filePath, hashedSlots.Display());
+            }
+            else
+            {
+                hashedSlots.AddNode(searchValue);
+                Console.WriteLine("Value {0} is not Present in Hash Table", searchValue);
+                File.WriteAllText(filePath, hashedSlots.Display());
             }
         }
     }
